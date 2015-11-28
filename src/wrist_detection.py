1da7a4492c3fd3610a8072dfc0b94ecbf69eee9a
Aditya Viswanathan <aditya@adityaviswanathan.com>
2015-11-28 01:26:59 -0600

    better wrist derivative heuristic

--- src/wrist_detection.py
+++ src/wrist_detection.py
@@ -25,7 +25,6 @@ def generateHandContour():
 	shapeMask = cv2.inRange(img, lower, upper)
 	_, contours,hierarchy=cv2.findContours(shapeMask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	global biggest_contour
-	print len(contours)
 	for i, c in enumerate(contours):
 		area = cv2.contourArea(c)
 		length = cv2.arcLength(c, False)
@@ -34,15 +33,13 @@ def generateHandContour():
 			biggest_contour = c
 
 def findWrist(contour):
-
 	top_base = int(img.shape[0] * 0.25) # ESTIMATE STARTING Y
-	bottom_base = int(img.shape[0] * 0.75) # ESTIMATE STARTING Y
-	# left_base = int(img.shape[1] * 0.5) # ESTIMATE STARTING X
+	bottom_base = int(img.shape[0] * 0.75) # ESTIMATE ENDING Y
 
 	# selected_contour_pts = [i for i in contour if i[0][1] < bottom_base and i[0][1] > top_base and i[0][0] > left_base]
-	selected_contour_pts = [i for i in contour if i[0][1] < bottom_base and i[0][1] > top_base]
-	flattened_selected_contour_pts = [item for sublist in selected_contour_pts for item in sublist]
-	flattened_selected_contour_pts = flattened_selected_contour_pts[::2]
+	selected_contour_pts = [i for i in contour if i[0][1] <= bottom_base and i[0][1] >= top_base]
+	flattened_selected_contour_pts_verbose = [item for sublist in selected_contour_pts for item in sublist]
+	flattened_selected_contour_pts = flattened_selected_contour_pts_verbose[::2]
 	derivatives = []
 	for i, pt1 in enumerate(flattened_selected_contour_pts):
 		cv2.circle(out_img, (pt1[0], pt1[1]), 4, colors[1], -1)
@@ -53,91 +50,29 @@ def findWrist(contour):
 			else:
 				derivatives.append(pt2[0] - pt1[0])
 
-	thresh = 100
-	# wrong_val = max(abs(v) for v in derivatives)
-	# wrong_val_2 = max(abs(v) for v in derivatives if abs(v) != wrong_val and abs(v) < thresh)
-	# val = max(abs(v) for v in derivatives if abs(v) != wrong_val_2 and abs(v) < thresh)
-	val = max(abs(v) for v in derivatives if  abs(v) < thresh)
+	found = [i for i, pt in enumerate(flattened_selected_contour_pts) if pt[1] == top_base]
+	min_val = None
+	max_val = None
+	for index in found:
+		if min_val is None or flattened_selected_contour_pts[index][0] < min_val:
+			min_val = flattened_selected_contour_pts[index][0]
+		if max_val is None or flattened_selected_contour_pts[index][0] > max_val:
+			max_val = flattened_selected_contour_pts[index][0]
+	thresh = (min_val + max_val) / 2
+	print 'DERIVATIVE THRESHOLD: %d' % thresh
+
+	val = max(abs(v) for v in derivatives if abs(v) < thresh)
 	index = -1
 	if val not in derivatives:
 		index = derivatives.index(-1 * val)
 	else:
 		index = derivatives.index(val)
-	print val
-	print flattened_selected_contour_pts[index-1]
-	print flattened_selected_contour_pts[index]
-	print flattened_selected_contour_pts[index+1]
 	cv2.line(out_img, (flattened_selected_contour_pts[index][0], flattened_selected_contour_pts[index][1]), (0, flattened_selected_contour_pts[index][1]), colors[2], 5)
 	global out_img_2
 	out_img_2 = img[flattened_selected_contour_pts[index][1]:img.shape[0], 0:img.shape[1]]
-	# x_list = [pt[0] for pt in flattened_selected_contour_pts]
-	# np_contour = np.array(x_list)
-	# print np_contour
-	# derivative = np.diff(np_contour)
-	# better_derivatives = copy.deepcopy(derivative)
-
-	# thresh = 500
-	# val = max(abs(v) for v in derivative if abs(v) < thresh)
-	# index = -1
-	# if val not in derivative.tolist():
-	# 	index = derivative.tolist().index(-1 * val)
-	# else:
-	# 	index = derivative.tolist().index(val)
-	# # index, val = max(enumerate(derivative), key=operator.itemgetter(1))
-	# print val
-	# print flattened_selected_contour_pts[index-1]
-	# print flattened_selected_contour_pts[index]
-	# print flattened_selected_contour_pts[index+1]
-
-	# cv2.circle(out_img_2, (flattened_selected_contour_pts[index][0], flattened_selected_contour_pts[index][1]), 10, colors[2], -1)
-	# cv2.circle(out_img_2, (flattened_selected_contour_pts[index+1][0], flattened_selected_contour_pts[index+1][1]), 10, colors[3], -1)
-	# cv2.line(out_img, (flattened_selected_contour_pts[index][0], flattened_selected_contour_pts[index][1]), (0, flattened_selected_contour_pts[index][1]), colors[2], 5)
-	# horizontals = {}
-	# invalid_y = set()
-
-	# for i1, p1 in enumerate(contour):
-	# 	if i1 < len(contour)-1:
-	# 		p2 = contour[i1+1]
-
-	# 		# print p1, p2
-
-	# 		# OPENCV POINT IS Y, X
-	# 		# cv2.circle(out_img, (p1[0][0], p1[0][1]), 5, (255,245,238), -1)
-	# 		line_pts = util.get_line(p1[0][1], p1[0][0], p2[0][1], p2[0][0])
-
-	# 		for point in line_pts:
-	# 			# Y, X
-	# 			cv2.circle(out_img, (point[1], point[0]), 3, (255,245,238), -1)
-
-	# 			if point[1] not in invalid_y:
-	# 				if point[1] not in horizontals:
-	# 					horizontals[point[1]] = []
-	# 				horizontals[point[1]].append(point[0])
-	# 				if len(horizontals[point[1]]) > 2:
-	# 					del horizontals[point[1]]
-	# 					invalid_y.add(point[1])
-
-				# if point[1] not in invalid_y:
-				# 	if point[1] not in horizontals: 
-				# 		horizontals[point[1]] = []
-				# 	if len(horizontals[point[1]]) < 2 and point[0] not in horizontals[point[1]]:
-				# 		horizontals[point[1]].append(point[0])
-				# 	else:
-				# 		# MORE THAN TWO Y HEIGHTS, DISREGARD
-	 		# 			del horizontals[point[1]]
-	 		# 			invalid_y.add(point[1])
-
-	# for y, x_arr in horizontals.iteritems():
-	# 	if len(x_arr) == 2:
-	# 		print (y, x_arr[0])
-	# 		print (y, x_arr[1])
-	# 		cv2.line(out_img, (y, x_arr[0]), (y, x_arr[1]), colors[0], 2)
-	# 		# return
 
 
 generateHandContour()
-# print 'HERES THE BIGGEST CONTOUR'
-# print biggest_contour
 findWrist(biggest_contour)
 plt.subplot(221),plt.imshow(img, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
