85ea030cdf369f1e49044133f0ba0c3f7121e554
Rishi Bedi <rbedi100@gmail.com>
2015-12-01 19:25:59 -0800

    feat(extraction) add do_bvg

--- matlab/export_edgelist.m
+++ matlab/export_edgelist.m
@@ -1,10 +1,12 @@
-function export_edgelist(edgelist, filepath)
+function export_edgelist(edgelist, filepath, imgsize)
     fid = fopen(filepath,'wt');
+    fprintf(fid, '%.0f, %.0f\n', imgsize(1), imgsize(2));
     for i = 1:length(edgelist)
         flattened_array = reshape(edgelist{i}', [1, numel(edgelist{i})]);
         stringified_array = sprintf('%.0f,' , flattened_array);
         stringified_array = stringified_array(1:end-1);
         fprintf(fid, '%s\n', stringified_array);
+%         fprintf('%s\n', stringified_array);
     end
     fclose(fid);
 end
--- matlab/find_minima.m
+++ matlab/find_minima.m
@@ -1,20 +1,46 @@
 % find local mimima along rows
-function vein_x_img = find_minima(isOctave, fil, vein_x_img)
+function vein_img = find_minima(isOctave, fil, vein_img)
+
+%   figure
+%   imshow(fil)
+%   hold on
   for i = 1:size(fil,1)
       if (isOctave)
-          [ymax,imax,ymin,imin] = extrema(cpp_smooth(fil(i,:), 9)); % previously 10
+            data = cpp_smooth(fil(i,:),2);
       else
-          [ymax,imax,ymin,imin] = extrema(smooth(fil(i,:), 9)); % previously 10
+            data = smooth(fil(i,:),2);
       end
+      
+      data_inv = 1.01*max(data) - data;
+      [~, imin, widths, prominences] = findpeaks(data_inv);
       
       y_len = length(imin);
       y = ones(1,y_len) * i;
-      %plot(imin,y,'g*', 'markers',1)
       
-      if (isOctave)
-          vein_x_img(sub2ind(size(vein_x_img), y', imin')) = 1;
-      else
-          vein_x_img(sub2ind(size(vein_x_img), y', imin)) = 1;
+%       plot(imin,y,'g*', 'markers',1)
+      
+      if(numel(imin) > 0)       
+          vein_img(sub2ind(size(vein_img), y(:), imin(:))) = 1;
       end
   end
+  
+  for i = 1:size(fil,2)
+      if (isOctave)
+          data = cpp_smooth(fil(:,i), 4); 
+      else
+          data = smooth(fil(:,i),4);
+      end
+      data_inv = 1.01*max(data) - data;
+      [~, imin] = findpeaks(data_inv);
+      
+      x_len = length(imin);
+      x = ones(1,x_len) * i;
+      
+%       plot(x,imin,'g*', 'markers',1)
+      
+      if(numel(imin) > 0)
+           vein_img(sub2ind(size(vein_img), imin(:), x(:))) = 1;
+      end
+  end
+  
 end
